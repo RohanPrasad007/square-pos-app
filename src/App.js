698cab79e5d9e13a9139a10e25c415ca3b19097a
RohanPrasad007 <007rohanprasad@gmail.com>
2023-05-23 21:31:17 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      hello world!
+    <div >
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-24 bg-gray-100 px-4 flex items-center'>
+        <img src="/logo.png" alt="Rohan Auto Care" className='h-[80%]'/>
+    </div>
+  )
+}
+
+export default Header
