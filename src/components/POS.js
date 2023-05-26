11e1b6a02e64ac0c68eecad6800e6556f331d0c8
RohanPrasad007 <007rohanprasad@gmail.com>
2023-05-27 05:25:28 +0530

    added stayle in table

--- src/components/POS.js
+++ src/components/POS.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import generatePDF from "../service/Invoice";
 import { Watch } from "react-loader-spinner";
 
@@ -14,6 +14,12 @@ function POS() {
     },
   ]);
   const [loading, setLoading] = useState(false);
+  const [activeRowIndex, setActiveRowIndex] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    document.getElementById("cell-0-description").focus();
+  }, []);
 
   const save = async () => {
     alert("do you save");
@@ -57,6 +63,11 @@ function POS() {
     updatedItems[rowIndex]["amount"] = amount.toFixed(2);
 
     setItems(updatedItems);
+    let total = 0;
+    updatedItems.forEach((element) => {
+      total += parseFloat(element["amount"]);
+    });
+    setTotalAmount(total);
   };
 
   const handleKeyPress = (e, rowIndex, field) => {
@@ -145,84 +156,116 @@ function POS() {
 
   return (
     <div className="p-4">
-      <table className="min-w-full divide-y divide-gray-200 ">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Description
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Quantity
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              MRP
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Discount %
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Amount
-            </th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {items.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <input
-                  id={`cell-${rowIndex}-${"description"}`}
-                  className="w-full"
-                  type="text"
-                  value={row.description}
-                  onChange={(e) =>
-                    handleInputChange(e, rowIndex, "description")
-                  }
-                  onKeyDown={(e) => handleKeyPress(e, rowIndex, "description")}
-                />
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <input
-                  id={`cell-${rowIndex}-${"quantity"}`}
-                  className="w-full"
-                  type="number"
-                  value={row.quantity}
-                  onChange={(e) => handleInputChange(e, rowIndex, "quantity")}
-                  onKeyDown={(e) => handleKeyPress(e, rowIndex, "quantity")}
-                />
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <input
-                  id={`cell-${rowIndex}-${"mrp"}`}
-                  className="w-full"
-                  type="number"
-                  value={row.mrp}
-                  onChange={(e) => handleInputChange(e, rowIndex, "mrp")}
-                  onKeyDown={(e) => handleKeyPress(e, rowIndex, "mrp")}
-                />
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <input
-                  id={`cell-${rowIndex}-${"discount"}`}
-                  className="w-full"
-                  type="number"
-                  value={row.discount}
-                  onChange={(e) => handleInputChange(e, rowIndex, "discount")}
-                  onKeyDown={(e) => handleKeyPress(e, rowIndex, "discount")}
-                />
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <input
-                  id={`cell-${rowIndex}-${"amount"}`}
-                  className="w-full"
-                  type="number"
-                  value={row.amount}
-                  onKeyDown={(e) => handleKeyPress(e, rowIndex, "amount")}
-                />
-              </td>
+      <div className="rounded-lg overflow-hidden divide-gray-200">
+        <table className="min-w-full divide-y">
+          <thead className="bg-[#E75B4E] text-white">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                Description
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                Quantity
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                MRP
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                Discount %
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                Amount
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y bg-[#EAEDF2]">
+            {items.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className={`h-10 ${
+                  activeRowIndex === rowIndex ? "bg-white rounded-xl" : ""
+                }`}
+              >
+                <td className="h-10 whitespace-nowrap">
+                  <input
+                    id={`cell-${rowIndex}-${"description"}`}
+                    className={`w-full h-full px-6  outline-none focus:border-2 focus:border-[#316789
+                    ] focus:shadow-2xl rounded-lg  ${
+                      activeRowIndex === rowIndex ? "bg-white" : "bg-[#EAEDF2]"
+                    }`}
+                    type="text"
+                    value={row.description}
+                    onFocus={() => setActiveRowIndex(rowIndex)}
+                    onChange={(e) =>
+                      handleInputChange(e, rowIndex, "description")
+                    }
+                    onKeyDown={(e) =>
+                      handleKeyPress(e, rowIndex, "description")
+                    }
+                  />
+                </td>
+                <td className="h-10 whitespace-nowrap">
+                  <input
+                    id={`cell-${rowIndex}-${"quantity"}`}
+                    className={`w-full h-full px-6  outline-none focus:border-2 focus:border-[#316789
+                    ] focus:shadow-2xl rounded-lg  ${
+                      activeRowIndex === rowIndex ? "bg-white" : "bg-[#EAEDF2]"
+                    }`}
+                    type="number"
+                    value={row.quantity}
+                    onFocus={() => setActiveRowIndex(rowIndex)}
+                    onChange={(e) => handleInputChange(e, rowIndex, "quantity")}
+                    onKeyDown={(e) => handleKeyPress(e, rowIndex, "quantity")}
+                  />
+                </td>
+                <td className="h-10 whitespace-nowrap">
+                  <input
+                    id={`cell-${rowIndex}-${"mrp"}`}
+                    className={`w-full h-full px-6  outline-none focus:border-2 focus:border-[#316789
+                    ] focus:shadow-2xl rounded-lg  ${
+                      activeRowIndex === rowIndex ? "bg-white" : "bg-[#EAEDF2]"
+                    }`}
+                    type="number"
+                    value={row.mrp}
+                    onFocus={() => setActiveRowIndex(rowIndex)}
+                    onChange={(e) => handleInputChange(e, rowIndex, "mrp")}
+                    onKeyDown={(e) => handleKeyPress(e, rowIndex, "mrp")}
+                  />
+                </td>
+                <td className="h-10 whitespace-nowrap">
+                  <input
+                    id={`cell-${rowIndex}-${"discount"}`}
+                    className={`w-full h-full px-6  outline-none focus:border-2 focus:border-[#316789
+                    ] focus:shadow-2xl rounded-lg  ${
+                      activeRowIndex === rowIndex ? "bg-white" : "bg-[#EAEDF2]"
+                    }`}
+                    type="number"
+                    value={row.discount}
+                    onFocus={() => setActiveRowIndex(rowIndex)}
+                    onChange={(e) => handleInputChange(e, rowIndex, "discount")}
+                    onKeyDown={(e) => handleKeyPress(e, rowIndex, "discount")}
+                  />
+                </td>
+                <td className="h-10 whitespace-nowrap">
+                  <input
+                    id={`cell-${rowIndex}-${"amount"}`}
+                    className={`w-full h-full px-6  outline-none focus:border-2 focus:border-[#316789
+                    ] focus:shadow-2xl rounded-lg  ${
+                      activeRowIndex === rowIndex ? "bg-white" : "bg-[#EAEDF2]"
+                    }`}
+                    type="number"
+                    value={row.amount}
+                    onKeyDown={(e) => handleKeyPress(e, rowIndex, "amount")}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="bg-[#316789] text-white text-xl py-4 px-6 flex justify-end gap-20 items-center">
+          <div>Total Amount</div>
+          <div>{totalAmount.toFixed(2)}</div>
+        </div>
+      </div>
       {loading && (
         <div className="flex justify-center items-center absolute top-0 left-0 w-screen	h-screen bg-gray-200 opacity-70">
           <Watch
